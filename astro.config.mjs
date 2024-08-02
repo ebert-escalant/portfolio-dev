e294d2e237eb127bacb484fe9b846cfc599d6c5c
ebert-escalant <ebertescalant@gmail.com>
2024-08-01 20:00:59 -0500

    feat: Add Astro robots.txt integration and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://ebert-escalant.vercel.app',
   devToolbar: {
 	enabled: false
   }
