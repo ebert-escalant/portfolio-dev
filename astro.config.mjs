fe9ff4596eef001f67c7048e54bebb48f9f3fae7
Ebert Escalante <ebertescalant@gmail.com>
2025-02-12 11:22:59 -0500

    feat: add sitemap integration and update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://ebert-escalant.vercel.app',
+	site: 'https://ebert-escalante.tech',
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   devToolbar: {
-	enabled: false
+    enabled: false
   }
 });
